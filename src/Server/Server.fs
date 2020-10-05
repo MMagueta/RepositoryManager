12a7493e9281e484491cfe6ea6c8cf157d06724e
MMagueta <maguetamarcos@gmail.com>
2020-10-05 20:23:32 -0300

    Initial commit

---
+++ src/Server/Server.fs
@@ -0,0 +1,41 @@
+namespace Repository
+
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Hosting
+
+open Giraffe
+open Saturn
+
+open Shared
+
+
+// run app
+
+(*
+    http://localhost:8085/
+*)
+module Server = 
+
+    let webApp =
+        router {
+            get "/" (["A";"B";"C"] |> json)
+            get Route.hello ("Hello from SAFE!" |> json)
+        }
+
+    let app =
+        application {
+            url "http://0.0.0.0:8085"
+            use_router webApp
+            memory_cache
+            use_static "public"
+            use_json_serializer (Thoth.Json.Giraffe.ThothSerializer())
+            use_gzip
+        }
+
+    let exitCode = 0
+
+    [<EntryPoint>]
+    let main args =
+        run app
+        exitCode
