4926fc02bf4b2f96780ef8c694e2853bfe2a920f
MMagueta <maguetamarcos@gmail.com>
2020-10-05 21:33:26 -0300

    feat: prices records model added

---
+++ src/Server/Models/PriceRecord.fs
@@ -0,0 +1,19 @@
+namespace Models
+
+open System.ComponentModel.DataAnnotations
+
+[<CLIMutable>]
+type PriceRecordItem =
+    {
+        Id : int
+        [<Required>]
+        Date : System.DateTime
+        [<Required>]
+        Price : float
+        [<Required>]
+        Quantity : int
+        [<Required>]
+        Provider : ProviderItem
+        [<Required>]
+        CPair : CurrencyPairItem
+    }
