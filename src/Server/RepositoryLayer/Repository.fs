88b67329482660a05b61ef82caf2adf647d7ae10
MMagueta <maguetamarcos@gmail.com>
2020-10-06 00:16:45 -0300

    feat: repository implementation with context and generic types association to interface

---
+++ src/Server/RepositoryLayer/Repository.fs
@@ -0,0 +1,14 @@
+namespace Repository
+
+open System
+open System.Collections.Generic
+open Microsoft.EntityFrameworkCore
+open System.Linq
+open System.Linq.Expressions
+
+open Context
+
+type Repository(contextIn : DbContext) = 
+    let context = contextIn
+
+    member this.Get<'T when 'T :> IRepository<'T>>(id : int) = context.Set<'T>().Find(id)
