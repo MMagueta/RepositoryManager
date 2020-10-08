d61a9c05a52a32110ddaf2d8dd251c16da76a571
MMagueta <maguetamarcos@gmail.com>
2020-10-08 02:15:38 -0300

    fix: fixed previous composition bug and convertion to list for json

--- src/Server/RepositoryLayer/PriceRecordsRepository.fs
+++ src/Server/RepositoryLayer/PriceRecordsRepository.fs
@@ -24,8 +24,9 @@ type PriceRecordsRepository(contextIn : DbContext) =
         context.Set<PriceRecordItem>() |> Seq.filter (fun x -> x.Quantity <= max_quantity)
     member this.GetByMinQuantity(min_quantity : int) = 
         context.Set<PriceRecordItem>() |> Seq.filter (fun x -> x.Quantity >= min_quantity)
-    member this.GetAllByDate = 
+    member this.GetAllByDate() = 
         context.Set<PriceRecordItem>()
-        |> Seq.groupBy (fun x -> x.SubProvider)
-        |> Seq.map (fun (key, values) -> (key, values |> Seq.map (fun x -> x.Date), values |> Seq.map (fun x -> x.Price) ))
-        |> Seq.map (fun (key, dates, prices) -> (key, dates |> Seq.sortBy (fun x -> x), prices ))
+        |> Seq.groupBy (fun x -> x.SubProvider) //Will add different records under a same subprovider
+        |> Seq.map (fun (key, values) -> (key,  values |> Seq.map (fun x -> (x.Date, x.Price) )))
+        |> Seq.map (fun (k,t) -> (k, t |> Seq.sortBy (fun (a,b) -> a ) |> List.ofSeq))
+        |> List.ofSeq
