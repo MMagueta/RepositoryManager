7acf54894669a79094d0ecba9b8a9d0924d3822b
MMagueta <maguetamarcos@gmail.com>
2020-10-08 01:00:10 -0300

    feat: new currency pairs repository with search by label

---
+++ src/Server/RepositoryLayer/CurrencyPairsRepository.fs
@@ -0,0 +1,16 @@
+namespace Repository
+
+open System
+open System.Collections.Generic
+open Microsoft.EntityFrameworkCore
+open System.Linq
+open System.Linq.Expressions
+
+open Context
+open Models
+
+type CurrencyPairsRepository(contextIn : DbContext) = 
+    inherit Repository(contextIn)
+    let context = contextIn
+    member this.GetByLabel(name : string) = 
+        context.Set<CurrencyPairItem>() |> Seq.filter (fun x -> x.Label = name)
