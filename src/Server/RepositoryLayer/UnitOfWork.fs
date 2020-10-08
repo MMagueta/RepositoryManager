d3c6db5a75fb848976519ba8cd193c5a08b85d76
MMagueta <maguetamarcos@gmail.com>
2020-10-08 19:06:01 -0300

    feat: unit of work with facade for testing

--- src/Server/RepositoryLayer/UnitOfWork.fs
+++ src/Server/RepositoryLayer/UnitOfWork.fs
@@ -2,13 +2,32 @@ namespace Repository
 
 open Context
 
-type UnitOfWork(contextIn : RepositoryContext) =
-    let context = contextIn
-    let providers = new Repository(context)
-    let crpairs = new Repository(context)
-    let pricerecords = new Repository(context)
+open System.Runtime.Serialization
+open System.Runtime.Serialization.Formatters.Binary
+open System.Runtime.Serialization.Json
+open System.IO
 
-    member this.Complete() =
-        context.SaveChanges() |> ignore
-    member this.Dispose =
-        context.Dispose() |> ignore
+open Newtonsoft.Json
+open Models
+open Giraffe
+
+module UnitOfWork = 
+    let ctx = ContextFactory.Create("Host=database-3.cnnri9trsf5s.us-east-2.rds.amazonaws.com;Port=5432;Pooling=true;Database=teste_pg;User Id=postgres;Password=159753123")
+    type UnitOfWork(contextIn : RepositoryContext) =
+        let context = contextIn
+        let providers = new ProvidersRepository(context)
+        let crpairs = new CurrencyPairsRepository(context)
+        let pricerecords = new PriceRecordsRepository(context)
+        
+        member this.Serialize obj = 
+            obj |> JsonConvert.SerializeObject |> json
+
+        member this.GetPriceRecordsByCPairs(cpairs_id : int) = 
+            pricerecords.GetByCPairs(cpairs_id)
+
+        member this.Complete() =
+            context.SaveChanges() |> ignore
+        member this.Dispose =
+            context.Dispose() |> ignore
+
+    let uow = UnitOfWork(ctx)
