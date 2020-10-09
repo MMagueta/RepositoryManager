4ac1af1708c82e7b67b71fafef885400732f4518
MMagueta <maguetamarcos@gmail.com>
2020-10-08 21:22:23 -0300

    refactor: pack and unpack functions added to PriceRecordsRepository.fs, might add to parent class later

--- src/Server/RepositoryLayer/PriceRecordsRepository.fs
+++ src/Server/RepositoryLayer/PriceRecordsRepository.fs
@@ -12,6 +12,10 @@ open Models
 type PriceRecordsRepository(contextIn : DbContext) = 
     inherit Repository(contextIn)
     let context = contextIn
+
+    let PackListSomeOrNone x = match x with | [] -> None | _ -> Some(x) 
+    let UnpackListSomeOrNone x = match x with | None -> [] | Some(x) -> x
+
     member this.GetByCPairs(cpairs_id : int) = 
         context.Set<PriceRecordItem>() |> Seq.tryFind (fun x -> x.CPair.Id = cpairs_id)
 
@@ -21,21 +25,21 @@ type PriceRecordsRepository(contextIn : DbContext) =
     member this.GetByMaxDate(max_date : DateTime) = 
         context.Set<PriceRecordItem>() |> Seq.filter (fun x -> x.Date <= max_date)
         |> List.ofSeq
-        |> fun (x) -> match x with | [] -> None | _ -> Some(x)
+        |> PackListSomeOrNone
 
     member this.GetByMinDate(min_date : DateTime) = 
         context.Set<PriceRecordItem>() |> Seq.filter (fun x -> x.Date <= min_date)
         |> List.ofSeq
-        |> fun (x) -> match x with | [] -> None | _ -> Some(x)
+        |> PackListSomeOrNone
 
     member this.GetByDateRange(min_date : DateTime, max_date : DateTime) = 
-        (this.GetByMinDate(min_date) |> fun (x) -> match x with | None -> [] | Some(x) -> x) @ (this.GetByMaxDate(max_date) |> fun (x) -> match x with | None -> [] | Some(x) -> x) |> List.distinct |> fun (x) -> match x with | [] -> None | _ -> Some(x) 
+        (this.GetByMinDate(min_date) |> fun (x) -> match x with | None -> [] | Some(x) -> x) @ (this.GetByMaxDate(max_date) |> UnpackListSomeOrNone ) |> List.distinct |> PackListSomeOrNone
 
     member this.GetByMaxQuantity(max_quantity : int) = 
-        context.Set<PriceRecordItem>() |> Seq.filter (fun x -> x.Quantity <= max_quantity)
+        context.Set<PriceRecordItem>() |> Seq.filter (fun x -> x.Quantity <= max_quantity) |> List.ofSeq |> PackListSomeOrNone
 
     member this.GetByMinQuantity(min_quantity : int) = 
-        context.Set<PriceRecordItem>() |> Seq.filter (fun x -> x.Quantity >= min_quantity)
+        context.Set<PriceRecordItem>() |> Seq.filter (fun x -> x.Quantity >= min_quantity) |> List.ofSeq |> PackListSomeOrNone
 
     member this.GetAllByDate() = 
         context.Set<PriceRecordItem>()
@@ -43,3 +47,4 @@ type PriceRecordsRepository(contextIn : DbContext) =
         |> Seq.map (fun (key, values) -> (key,  values |> Seq.map (fun x -> (x.Date, x.Price) )))
         |> Seq.map (fun (k,t) -> (k, t |> Seq.sortBy (fun (a,b) -> a ) |> List.ofSeq))
         |> List.ofSeq
+        |> PackListSomeOrNone
