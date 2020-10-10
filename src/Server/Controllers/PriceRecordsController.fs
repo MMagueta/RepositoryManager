790f18322c80e78c0af626bd7239ffbb434671a5
MMagueta <maguetamarcos@gmail.com>
2020-10-10 16:45:36 -0300

    style: uniform names for functions and params

--- src/Server/Controllers/PriceRecordsController.fs
+++ src/Server/Controllers/PriceRecordsController.fs
@@ -13,37 +13,37 @@ open Repository.UnitOfWork
 open System.Linq
 
 module Controller =
-    let match_pattern (matcher)= 
+    let MatchPattern(matcher)= 
         match matcher with
         | Some(record) -> record |> json
         | None -> setStatusCode 404 >=> text "Records not found"
 
-    let FindPriceRecordsByCPairs cpairs_id =
-        cpairs_id |> uow.GetPriceRecordsByCPairs |> match_pattern
+    let FindPriceRecordsByCPairs(cpairs_id) =
+        cpairs_id |> uow.GetPriceRecordsByCPairs |> MatchPattern
 
-    let GetPriceRecordsByProviders providers_id =
-        providers_id |> uow.GetPriceRecordsByProviders |> match_pattern
+    let GetPriceRecordsByProviders(providers_id) =
+        providers_id |> uow.GetPriceRecordsByProviders |> MatchPattern
 
-    let GetByDateRange (min_date, max_date) = 
-        (uow.GetByDateRange (min_date |> System.DateTime.Parse, max_date |> System.DateTime.Parse)) |> match_pattern
+    let GetByDateRange(min_date, max_date) = 
+        (uow.GetByDateRange (min_date |> System.DateTime.Parse, max_date |> System.DateTime.Parse)) |> MatchPattern
 
-    let FilterByMinDate (min_date) = 
-        min_date |> System.DateTime.Parse |> uow.FilterByMinDate |> match_pattern
+    let FilterByMinDate(min_date) = 
+        min_date |> System.DateTime.Parse |> uow.FilterByMinDate |> MatchPattern
 
-    let FilterByMaxQuantity (max_qtd) = 
-        max_qtd |> uow.FilterByMaxQuantity |> match_pattern
+    let FilterByMaxQuantity(max_qtd) = 
+        max_qtd |> uow.FilterByMaxQuantity |> MatchPattern
 
-    let FilterByMinQuantity (min_qtd) = 
-        min_qtd |> uow.FilterByMinQuantity |> match_pattern
+    let FilterByMinQuantity(min_qtd) = 
+        min_qtd |> uow.FilterByMinQuantity |> MatchPattern
 
-    let FilterByMaxDate (max_date) = 
-        max_date |> System.DateTime.Parse |> uow.FilterByMaxDate |> match_pattern
+    let FilterByMaxDate(max_date) = 
+        max_date |> System.DateTime.Parse |> uow.FilterByMaxDate |> MatchPattern
 
-    let GetAllPricesInOrderByDate (min_date, max_date) = 
-        (uow.GetAllPricesInOrderByDate (min_date |> System.DateTime.Parse, max_date |> System.DateTime.Parse)) |> match_pattern
+    let GetAllPricesInOrderByDate(min_date, max_date) = 
+        (uow.GetAllPricesInOrderByDate (min_date |> System.DateTime.Parse, max_date |> System.DateTime.Parse)) |> MatchPattern
 
     let GetMarketData(min_date, max_date, currency_pair_id, provider_id) = 
-        (uow.GetMarketData (min_date |> System.DateTime.Parse, max_date |> System.DateTime.Parse, provider_id, currency_pair_id)) |> match_pattern
+        (uow.GetMarketData (min_date |> System.DateTime.Parse, max_date |> System.DateTime.Parse, provider_id, currency_pair_id)) |> MatchPattern
 
     //----------------------- POST -----------------------
 
