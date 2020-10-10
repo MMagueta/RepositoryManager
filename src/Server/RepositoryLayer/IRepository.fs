b61fa74df37d3c54e1d123656078410ee1d4ac20
MMagueta <maguetamarcos@gmail.com>
2020-10-10 07:14:09 -0300

    feat: where clause supporting lambda expressions for refactor

--- src/Server/RepositoryLayer/IRepository.fs
+++ src/Server/RepositoryLayer/IRepository.fs
@@ -2,9 +2,11 @@ namespace Repository
 
 open System
 open System.Collections.Generic
+open System.Linq
 open System.Linq.Expressions
 
 type IRepository<'T> = 
     abstract GetById : int -> 'T
     abstract Add : 'T -> unit
     abstract Remove : 'T -> unit
+    abstract Where : Expression<Func<'T, bool>> -> IQueryable<'T>
--- src/Server/RepositoryLayer/Repository.fs
+++ src/Server/RepositoryLayer/Repository.fs
@@ -14,3 +14,4 @@ type Repository(contextIn : DbContext) =
     member this.GetById<'T>(id : int) = context.Set<'T>().Find(id)
     member this.Add<'T>(object : 'T) = context.Set<'T>().Add(object)
     member this.Remove<'T>(object : 'T) = context.Set<'T>().Remove(object)
+    member this.Where<'T>(expression : Expression<Func<'T, bool>>) = context.Set<'T>().Where(expression)
